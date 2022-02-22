2eb92919bb47f21292af6cbb6ff73a25b8e28d4e
barxtex1 <bartosz.popielarczyk@student.put.poznan.pl>
2022-02-22 14:57:02 +0100

    tasks 1-3 done

--- Lab 04 - Projekt blok/main.py
+++ Lab 04 - Projekt blok/main.py
@@ -8,14 +8,20 @@ def import_data():
         simple_data = pd.DataFrame(pd.read_csv("names/yob" + str(i) + ".txt", names=["name", "sex", "number"],
                                                index_col=0))
         all_data = pd.concat([all_data, simple_data])
-    data_ = pd.pivot_table(all_data, values="number", index=["sex", "name"], aggfunc=np.sum, sort="number")
+    data_ = pd.pivot_table(all_data, values="number", index=["sex", "name"], aggfunc=np.sum)
     return data_
 
 
-def processing(df):
-    print(df.sort_values("number"))
-    print(df.loc[df["number"] == 1, :])
 
+def new_frequency_columns(df):
+    # df["frequency_male"] =
+    return df
+
+
+def processing(df):
+    print("Liczba różnych (unikalnych) imion: ",len(df.loc[df["number"] == 5, :]))  # "To safeguard privacy, we restrict our list of names to those with at least 5 occurrences."
+    print("Liczba różnych (unikalnych) imion rozróżniając płeć: male -", len(df.loc[("M", df["number"] == 5), :]), "female -", len(df.loc[("F", df["number"] == 5), :]))
+    df = new_frequency_columns(df)
 
 if __name__ == '__main__':
     data = import_data()
