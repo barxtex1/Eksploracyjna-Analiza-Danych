e7b85668bbeeddc8d6e13b56c2f14abbd732e52f
barxtex1 <bartosz.popielarczyk@student.put.poznan.pl>
2022-03-01 12:43:17 +0100

    Start task 6

--- Lab 04 - Projekt blok/main.py
+++ Lab 04 - Projekt blok/main.py
@@ -16,7 +16,16 @@ def plot_graph(count_of_born, birthrate_sex):
     fig, axs = plt.subplots(2, 1)
     axs[0].plot(czas, count_of_born, '-b')
     axs[1].plot(czas, birthrate_sex, '-g')
+    for i in range(len(count_of_born)):
+        if birthrate_sex[i] == max(birthrate_sex):
+            print("Największy stosunek liczby narodzin dziewczynek do liczby narodzin chłopców był w roku", 1880+int(i))
+        elif birthrate_sex[i] == min(birthrate_sex):
+            print("Najmniejszy stosunek liczby narodzin dziewczynek do liczby narodzin chłopców był w roku", 1880+int(i))
     plt.show()
+
+
+def most_popular_names(df):
+    print("xxx")
 
 
 def import_data():
@@ -31,14 +40,14 @@ def import_data():
         birthrate_sex_.append(np.sum(simple_data.loc["F", "number"]) / np.sum(simple_data.loc["M", "number"]))
         all_data = pd.concat([all_data, simple_data])
     data_ = pd.pivot_table(all_data, values=["number", "frequency_male", "frequency_female"], index=["sex", "name"], aggfunc=np.sum)
-    plot_graph(np.array(count_of_born_), np.array(birthrate_sex_))
+    # plot_graph(np.array(count_of_born_), np.array(birthrate_sex_))
     return data_
 
 
 def processing(df):
     print("Liczba różnych (unikalnych) imion: ", len(df.loc[df["number"] == 5, :]))  # "To safeguard privacy, we restrict our list of names to those with at least 5 occurrences."
     print("Liczba różnych (unikalnych) imion rozróżniając płeć: male -", len(df.loc[("M", df["number"] == 5), :]), "female -", len(df.loc[("F", df["number"] == 5), :]))
-    print(df)
+    # print(df)
 
 
 if __name__ == '__main__':
